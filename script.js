4824342102e9870fec323a5bfc79a44d1403fa6e
thumbnailsbysamad <thumbnailsbysamad@gmail.com>
2025-04-25 21:35:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ function redirectToPayment() {
     total += item.price * item.qty;
   });
 
-  const upiID = "9959864292@yesfam";
+  const upiID = "9959864292@upi";
   const upiLink = `upi://pay?pa=${upiID}&pn=${encodeURIComponent(name)}&am=${total}&cu=INR`;
 
   // Remove previous payment section if it exists
@@ -137,7 +137,7 @@ function redirectToPayment() {
   div.className = "mt-6 text-center px-4";
   div.innerHTML = `
     <p class="mb-2 font-bold text-green-600 text-base">Scan to Pay:</p>
-    <img src="images/Screenshot_2025-04-25-21-23-16-178_com.fampay.in-edit.jpg" alt="My UPI QR Code"
+    <img src="images/QR_1745596929.png" alt="My UPI QR Code"
          class="mx-auto rounded-xl shadow border border-gray-300"
          style="width: 150px; height: 150px; object-fit: contain;" />
     
